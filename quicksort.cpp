f58a80d63938bf9b8aa7cbdda83dc1018b8690ff
Tianjiao Huang <maitian519@gmail.com>
2019-02-15 22:37:11 -0800

    layout quicksort_parallel

--- quicksort.cpp
+++ quicksort.cpp
@@ -2,15 +2,51 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
 #include "CTPL/ctpl_stl.h"
 
 using std::vector;
 using std::string;
 
 void quicksort_parallel(int id, ctpl::thread_pool* threadpool, const std::string in_filename, const std::string out_filename) {
+    // If the file is empty, no more sorting is needed to be done
+    // This base case is very naive. But optimizting it is not in the scope of this project
+    std::ifstream ifile(in_filename);
+    const bool empty_input = ifile.peek() == std::ifstream::traits_type::eof();
+    if (empty_input) return;
 
+    // Choose a pivot
+    // Again, this is really naive, but whatever
+    string pivot;
+    std::getline(ifile, pivot);
+
+    // Partition the data
+    // x goes into large_partition_file if x >= pivot.
+    // Otherwise, x goes into small_partition_file.
+
+    string line;
+    while(std::getline(ifile, line)) {
+        
+    }
+
+    // Push jobs into queue
+
+    // Merge results
 }
 
+
+
 std::vector<std::string> split_string_by_comma(const string& str) {
-    return {};
+    vector<string> rtn;
+    string temp;
+    for (const char& c : str) {
+        if (c != ',' || c != ' ') {
+            temp += c;
+        } else {
+            if (!temp.empty()) {
+                rtn.push_back(temp);
+            }
+        }
+    }
+    return rtn;
 }
--- quicksort.hpp
+++ quicksort.hpp
@@ -5,9 +5,18 @@
 #include <vector>
 #include "CTPL/ctpl_stl.h"
 
+enum DataType {
+    NUMBER,
+    STRING
+};
+
+// Do quick sort on `in_filename` then write output to `out_filename`
+void quicksort_parallel(int id, ctpl::thread_pool* threadpool, const std::string in_filename, const std::string out_filename = "");
+
 // Helpers
 std::vector<std::string> split_string_by_comma(const std::string& str);
+// Get data type of a row
+std::vector<DataType> get_datatype(const std::vector<std::string>& row);
 
-void quicksort_parallel(int id, ctpl::thread_pool* threadpool, const std::string in_filename, const std::string out_filename = "");
 
 #endif // QUICKSORT_HPP
